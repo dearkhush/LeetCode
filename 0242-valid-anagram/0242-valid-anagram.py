8075a2e5ed6b217ee37c2bcf925546e5601d0c47
dearkhush <kkhushboovermaa@gmail.com>
2026-05-22 14:47:08 +0530

    Time: 11 ms (76.58%), Space: 19.4 MB (43.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        d1={}
+        d2={}
+        for ch in s:
+            if ch not in d1:
+                d1[ch]=1
+            else:
+                d1[ch]+=1
+        for ch in t:
+            if ch not in d2:
+                d2[ch]=1
+            else:
+                d2[ch]+=1
+
+        return d1==d2
+
+
+
+        
+
